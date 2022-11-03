1c7b2b7418314ca2a5f88a54b85b380a4fef34a2
Italo Souza <47384565+DIMAAGR@users.noreply.github.com>
2022-11-03 00:20:18 -0300

    adicionado tela game_view.dart

---
+++ lib/src/views/game_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:snake_game/src/core/core.dart';
+
+class GameView extends StatelessWidget {
+  const GameView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
