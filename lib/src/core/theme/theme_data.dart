37e3e9405ddeec0bdd7355af1622e0c64cb5b61a
Italo Souza <47384565+DIMAAGR@users.noreply.github.com>
2022-11-03 00:24:48 -0300

    criado pacote de temas para dark e white theme colors

---
+++ lib/src/core/theme/theme_data.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ApplicationTheme {
+  ThemeData get whiteTheme => ThemeData(backgroundColor: const Color.fromARGB(255, 250, 250, 250));
+  ThemeData get darkTheme => ThemeData(backgroundColor: const Color.fromARGB(255, 40, 40, 40));
+}
